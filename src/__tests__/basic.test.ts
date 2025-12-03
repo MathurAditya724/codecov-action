bfec0bac51d58cb6f2a1c4cfafc079137effda6d
mathuraditya724 <mathuraditya724@gmail.com>
2025-12-04 02:43:03 +0530

    fix: minor changes

--- src/__tests__/basic.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { add } from "./operations";
-
-describe("Basic operations", () => {
-  it("should add two numbers", () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
