c1b45d202eeac036227bd4c13835196292105f52
mathuraditya724 <mathuraditya724@gmail.com>
2025-12-03 15:49:10 +0530

    wip

--- src/types/test-results.ts
+++ src/types/test-results.ts
@@ -1,47 +1,46 @@
 export interface TestCase {
-	classname: string;
-	name: string;
-	time: number;
-	failure?: TestFailure;
-	skipped?: boolean;
+  classname: string;
+  name: string;
+  time: number;
+  failure?: TestFailure;
+  skipped?: boolean;
 }
 
 export interface TestFailure {
-	message: string;
-	type?: string;
-	content?: string; // Stack trace or error details
+  message: string;
+  type?: string;
+  content?: string; // Stack trace or error details
 }
 
 export interface TestSuite {
-	name: string;
-	tests: number;
-	failures: number;
-	errors: number;
-	skipped: number;
-	time: number;
-	testcases: TestCase[];
+  name: string;
+  tests: number;
+  failures: number;
+  errors: number;
+  skipped: number;
+  time: number;
+  testcases: TestCase[];
 }
 
 export interface TestResults {
-	name?: string;
-	tests: number;
-	failures: number;
-	errors: number;
-	skipped: number;
-	time: number;
-	testsuites: TestSuite[];
+  name?: string;
+  tests: number;
+  failures: number;
+  errors: number;
+  skipped: number;
+  time: number;
+  testsuites: TestSuite[];
 }
 
 export interface AggregatedTestResults {
-	totalTests: number;
-	passedTests: number;
-	failedTests: number;
-	skippedTests: number;
-	totalTime: number;
-	passRate: number;
-	failedTestCases: Array<{
-		suiteName: string;
-		testCase: TestCase;
-	}>;
+  totalTests: number;
+  passedTests: number;
+  failedTests: number;
+  skippedTests: number;
+  totalTime: number;
+  passRate: number;
+  failedTestCases: Array<{
+    suiteName: string;
+    testCase: TestCase;
+  }>;
 }
-
